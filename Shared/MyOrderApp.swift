26540cc93dc675537d9e9df7401593bd5ff8d9b7
satvikkathpal <satvikkathpal0109@gmail.com>
2021-10-26 12:21:40 -0400

    Initial Commit

---
+++ Shared/MyOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOrderApp.swift
+//  Shared
+//
+//  Created by Satvik Kathpal on 2021-10-26.
+//
+
+import SwiftUI
+
+@main
+struct MyOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
