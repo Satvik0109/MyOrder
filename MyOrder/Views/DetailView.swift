67ecd3a7995f4d7c034ce82cf945f9212586f301
satvikkathpal <satvikkathpal0109@gmail.com>
2021-10-30 12:01:35 -0400

    View: DetailView updated

--- MyOrder/Views/DetailView.swift
+++ MyOrder/Views/DetailView.swift
@@ -9,8 +9,50 @@ import SwiftUI
 
 struct DetailView: View {
     let selectedOrderIndex : Int
+    @State private var type: String = ""
+    @State private var size: String = ""
+    @State private var qty: String = ""
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var myOrderHelper : MyOrderHelper
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack(alignment: .leading){
+            Text("Order Details")
+                .font(.title)
+                .padding(20)
+            
+            Form{
+                TextField("Quantity", text: self.$qty)
+            }//Form
+            
+            Button(action: {
+                self.updateOrder()
+            }){
+                Text("Update Order")
+            }
+            Spacer()
+        }//VStack
+        .frame(maxWidth: .infinity)
+        .onAppear(){
+            self.type = self.myOrderHelper.orderList[selectedOrderIndex].type
+            self.size = self.myOrderHelper.orderList[selectedOrderIndex].size
+            self.qty = self.myOrderHelper.orderList[selectedOrderIndex].quantity
+        }
+        .onDisappear(){
+            self.myOrderHelper.orderList.removeAll()
+            self.myOrderHelper.getAllOrders()
+            print(#function, "onDisappear DetailView(): \(self.myOrderHelper.orderList)")
+        }
+            
+    }
+    private func updateOrder(){
+        self.myOrderHelper.orderList[selectedOrderIndex].type = self.type
+        self.myOrderHelper.orderList[selectedOrderIndex].size = self.size
+        self.myOrderHelper.orderList[selectedOrderIndex].quantity = self.qty
+        
+        self.myOrderHelper.updateOrder(updatedOrder: self.myOrderHelper.orderList[selectedOrderIndex])
+        self.presentationMode.wrappedValue.dismiss()
     }
 }
 
